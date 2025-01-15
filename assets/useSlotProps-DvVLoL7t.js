1ac395342ec0462e1beb2cef1bba3ccb55eaacbb
Carq <lciura.pl@gmail.com>
2025-01-15 21:03:22 +0100

    Updates

--- assets/useSlotProps-DvVLoL7t.js
+++
@@ -1,1 +0,0 @@
-import{as as u,at as P,u as S,au as c}from"./index-BJGmpdve.js";function g(e){var s;const{elementType:t,externalSlotProps:n,ownerState:r,skipResolvingSlotProps:p=!1,...a}=e,o=p?{}:u(n,r),{props:l,internalRef:f}=P({...a,externalSlotProps:o}),i=S(f,o==null?void 0:o.ref,(s=e.additionalProps)==null?void 0:s.ref);return c(t,{...l,ref:i},r)}export{g as u};
